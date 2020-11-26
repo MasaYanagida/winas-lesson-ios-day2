b00eae615d199fde194f3bc2c8fa3527c9df4f11
MasaYanagida <kyoyah.rod@gmail.com>
2020-11-26 20:27:49 +0900

    Set components

--- Day2Homework/Day2Homework/ViewController.swift
+++ Day2Homework/Day2Homework/ViewController.swift
@@ -13,4 +13,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    @IBOutlet private dynamic weak var iconView: UIImageView!
+    @IBOutlet private dynamic weak var nameLabel: UILabel!
+    @IBOutlet private dynamic weak var addressLabel: UILabel!
+    @IBOutlet private dynamic weak var contentsLabel: UILabel!
+    @IBOutlet private dynamic weak var favoriteButton: UIButton!
+    @IBOutlet private dynamic weak var imageView: UIImageView!
+    @IBOutlet private dynamic weak var heightConstraint: NSLayoutConstraint!
+    @IBAction private func buttonTouchUpInside(_ sender: UIButton) {
+        print("touch!!")
+    }
 }
