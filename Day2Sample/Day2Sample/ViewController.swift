596e0590c2dbb75b6d1e1b1bc442ab9afaa381e4
MasaYanagida <kyoyah.rod@gmail.com>
2020-11-27 19:28:10 +0900

    Update samples

--- Day2Sample/Day2Sample/ViewController.swift
+++ Day2Sample/Day2Sample/ViewController.swift
@@ -10,16 +10,59 @@ import SnapKit
 
 class ViewController: UIViewController {
     
+    // IMPORTANT : ビューをプログラムから生成するときは、`lazy`で行うのが一般的です。このプロパティをcallしたときに初めて＆一回だけ生成されるからです
+    private lazy var button: UIButton = {
+        let button = UIButton()
+        button.cornerRadius = 32
+        button.borderWidth = 1
+        button.borderColor = .red
+        button.backgroundColor = .white
+        button.setTitleColor(UIColor.hexColor(0xff0000), for: .normal)
+        button.setTitle("画像拡大/縮小", for: .normal)
+        button.addTarget(self, action: #selector(buttonTouchUpInside2(sender:)), for: .touchUpInside)
+        view.addSubview(button)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // IMPORTANT : SnapKitでレイアウト制約をつけます
+        button.snp.makeConstraints { make in
+            make.bottom.equalTo(view).offset(-20)
+            make.left.equalTo(view).offset(20)
+            make.right.equalTo(view).offset(-20)
+            make.height.equalTo(64)
+        }
     }
+    var isExpand: Bool = false
+    
+    @IBOutlet private dynamic weak var loginButton: UIButton!
     @IBOutlet private dynamic weak var heightConstraint: NSLayoutConstraint!
     @IBAction private func buttonTouchUpInside(_ sender: UIButton) {
-        print("touch!!")
+        toggle()
+    }
+    private func toggle() {
+        heightConstraint.constant = isExpand ? 128 : 256
+        UIView.animate(
+            withDuration: 0.6,
+            delay: 0,
+            usingSpringWithDamping: 0.7,
+            initialSpringVelocity: 0.5,
+            options: .curveLinear,
+            animations: { [weak self] in
+                self?.view.layoutIfNeeded()
+            },
+            completion: { [weak self] _ in
+                guard let `self` = self else { return }
+                self.isExpand = !self.isExpand
+            }
+        )
     }
     @IBAction private func onImageViewTapped(sender: UITapGestureRecognizer) {
-        print(#function)
+        toggle()
     }
     @objc private func buttonTouchUpInside2(sender: UIButton) {
+        toggle()
     }
 }
