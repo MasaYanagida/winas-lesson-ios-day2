ab92b37f46e85ef2f939807b7b53a6d5e8b11f50
田知勲 <j-jihoon@winas.jp>
2020-12-01 10:27:24 +0900

    書き方整理

--- Day2Homework/Day2Homework/ViewController.swift
+++ Day2Homework/Day2Homework/ViewController.swift
@@ -36,30 +36,37 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         initView()
-        nameLabel.text = contents.name
-        addressLabel.text = contents.address
-        introductionLabel.text = contents.introduction
+
     }
 }
 
 extension ViewController {
     private func initView() {
+        // bottom button
         button.snp.makeConstraints { make in
             make.bottom.equalTo(view).offset(-20)
             make.left.equalTo(view).offset(20)
             make.right.equalTo(view).offset(-20)
             make.height.equalTo(64)
         }
+        
         imageView.layer.cornerRadius = 10
         showFavoriteButton.layer.cornerRadius = showFavoriteButton.frame.height / 2
         showFavoriteButton.addTarget(self, action: #selector(tappedShowFavoriteButton(sender:)), for: .touchUpInside)
         
+        //label
+        nameLabel.text = contents.name
+        addressLabel.text = contents.address
+        introductionLabel.text = contents.introduction
+        
+        //showFavoriteButton
         if contents.showFavoriteButton == false {
             showFavoriteButton.isHidden = true
         } else {
             showFavoriteButton.isHidden = false
         }
     }
+    
     @objc func buttonTouchUpInside(sender: UIButton) {
         heightConstraint.constant = isExpand ? 128 : 256
         UIView.animate(
@@ -77,6 +84,7 @@ extension ViewController {
             }
         )
     }
+    
     @objc func tappedShowFavoriteButton(sender: UIButton) {
         let alertController = UIAlertController(title: "", message: "お気に入りに登録しました", preferredStyle: .alert)
         let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
